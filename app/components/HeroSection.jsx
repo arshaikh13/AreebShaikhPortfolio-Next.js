a717e204f50df3a15bf305a6674ce70f913ac325
arshaikh13 <shaikha1426@gmail.com>
2025-08-27 19:52:06 -0400

    fixed resume button on certain browsers

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -61,8 +61,8 @@ const HeroSection = () => {
                             </button>
                         </Link>
                         <Link href="https://drive.google.com/file/d/19DHnLvt6QpRdg9utaU_K1hYzjlJyq41b/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                            <button className="px-1 py-1 cursor-pointer font-semibold rounded-full w-full sm:w-fit bg-gradient-to-br from-sky-800 via-cyan-400 to-cyan-700 hover:bg-slate-800 border-white mt-3">
-                                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                            <button className="px-1 py-1 cursor-pointer font-semibold rounded-full w-full sm:w-fit bg-gradient-to-br from-sky-800 via-cyan-400 to-cyan-700 hover:bg-slate-800 border-white mt-3 text-white">
+                                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 text-white">
                                     View Resume
                                 </span>
                             </button>
